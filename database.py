01f46090fff4c1b03ed098559c04b985520260f8
LuizOctavioSB <luizoctavios14@gmail.com>
2025-02-18 18:26:15 -0300

    Desafio aula 7

--- database.py
+++ database.py
@@ -3,26 +3,21 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Carregar variáveis de ambiente locais
 if os.getenv("GITHUB_ACTIONS") is None:
     load_dotenv(".env.local")
 
-# Definir valores padrão para evitar `None`
-user = os.getenv("POSTGRES_USER", "postgres")
-password = os.getenv("POSTGRES_PASSWORD", "123")
-database = os.getenv("POSTGRES_DB", "prototipo")
-host = os.getenv("POSTGRES_HOST", "localhost")  # Padrão correto
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+database = os.getenv("POSTGRES_DB")
+host = os.getenv("POSTGRES_HOST") 
 
-# Criar DATABASE_URL a partir das variáveis ou do Secret
-DATABASE_URL = os.getenv("DATABASE_URL", f"postgresql://{user}:{password}@{host}:5432/{database}")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
 
-if DATABASE_URL is None or "None" in DATABASE_URL:
-    raise ValueError("Erro: DATABASE_URL não está definida corretamente!")
 
-# Criar engine do SQLAlchemy
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
