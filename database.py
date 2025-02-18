daaab76186599dff23070f803a0924d815c329b2
LuizOctavioSB <luizoctavios14@gmail.com>
2025-02-18 10:05:09 -0300

    Desafio aula 7

--- database.py
+++ database.py
@@ -1,13 +1,22 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-user = "postgres"
-password = "123"
-database = "prototipo"
-host = "localhost"
+if os.getenv("GITHUB_ACTIONS") is None:
+    load_dotenv(".env.local")
+
+user = os.getenv("POSTGRES_USER")
+password = os.getenv("POSTGRES_PASSWORD")
+database = os.getenv("POSTGRES_DB")
+host = os.getenv("POSTGRES_HOST") 
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}/{database}"
+
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+
 SessionLocal = sessionmaker( autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
--- main.py
+++ main.py
@@ -1,28 +1,55 @@
-from fastapi import FastAPI
-from fastapi.params import Body
-from pydantic import BaseModel
+from fastapi import FastAPI, status, Depends
 import classes
 import model
-from database import engine
+from database import engine, get_db
+from typing import List
+from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
+# from scraping import scraping_ufu
 
 model.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-class Mensagem(BaseModel):
-    titulo: str
-    conteudo: str
-    publicada: bool = True
+origins = [
+ 'http://localhost:3000'
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "lala"}
 
+@app.post("/criar", status_code=status.HTTP_201_CREATED)
+def criar_valores(nova_mensagem: classes.Mensagem, db: Session = Depends(get_db)):
+    mensagem_criada = model.Model_Mensagem(titulo=nova_mensagem.titulo,
+    conteudo=nova_mensagem.conteudo, publicada=nova_mensagem.publicada)
+    mensagem_criada = model.Model_Mensagem(**nova_mensagem.model_dump())
+    db.add(mensagem_criada)
+    db.commit()    
+    db.refresh(mensagem_criada)
+    return {"Mensagem": mensagem_criada}
+
 @app.get("/quadrado/{num}")
 def square(num: int):
-    return num ** 3
+    return num ** 2
 
-@app.post("/criar")
-def criar_valores(nova_mensagem: Mensagem):
-    print(nova_mensagem)
-    return {"Mensagem": f"Título: {nova_mensagem.titulo} Conteúdo: {nova_mensagem.conteudo} Publicada: {nova_mensagem.publicada}"}
+@app.get("/mensagens", response_model=List[classes.Mensagem], status_code=status.HTTP_200_OK)
+async def buscar_valores(db: Session = Depends(get_db), skip: int = 0, limit: int=100):
+    mensagens = db.query(model.Model_Mensagem).offset(skip).limit(limit).all()
+    return mensagens
+
+# @app.get("/scraping", status_code=201)
+# def executar_scrape(db: Session = Depends(get_db)):
+#     return scraping_ufu(db)
+
+# @app.get("/menus", response_model=List[classes.MenuNav])
+# def buscar_menus(db: Session = Depends(get_db)):
+#     menus = db.query(model.Model_MenuNav).all()
+#     return menus
